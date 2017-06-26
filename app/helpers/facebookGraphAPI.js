db18d5dc9248cdeb99970aafb93aa2d8b8803524
Thomas Wiles <thomas.wiles@valetwaste.com>
2017-06-26 08:01:22 -0400

    Trying to create a single button tile

--- app/helpers/facebookGraphAPI.js
+++ app/helpers/facebookGraphAPI.js
@@ -58,15 +58,6 @@ module.exports = {
               "title": "Postback",
               "payload": "Payload for first element in a generic bubble",
             }],
-          },{
-            "title": "Second card",
-            "subtitle": "Element #2 of an hscroll",
-            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-            "buttons": [{
-              "type": "postback",
-              "title": "Postback",
-              "payload": "Payload for second element in a generic bubble",
-            }],
           }]
         }
       }
